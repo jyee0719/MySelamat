1a059a53da1d286d0f78e0be86451053497cafa9
Felice Yeoh <ysiowpoo@gmail.com>
2021-09-03 01:42:48 +0800

    HIHI

--- app/src/main/java/my/edu/utar/myselamat/HealthStatusQuestionActivity.java
+++ app/src/main/java/my/edu/utar/myselamat/HealthStatusQuestionActivity.java
@@ -144,7 +144,7 @@ public class HealthStatusQuestionActivity extends AppCompatActivity {
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HealthStatusQuestionActivity.this, HealthStatusActivity.class);
+                Intent intent = new Intent(HealthStatusQuestionActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
